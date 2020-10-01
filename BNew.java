dc6062162407ac1cb699fe44ae379458dec4602f
jpantiuchina <patrankyte@gmail.com>
2020-10-02 01:33:01 +0200

    add 3 methods to BNew.java

--- BNew.java
+++ BNew.java
@@ -1,15 +1,23 @@
 // This is a very cool class.
 //second comment line
-class B {
+class BNew {
   
   int field1 = 99999;
   
 
-  public B() {
+  public BNew() {
   }
 
   @Override
   public String toString() {
     return "B";
   }
+
+  public static void b1(){};
+
+  public static void b2(){};
+
+  public static void b3(){};
+
+
 }
