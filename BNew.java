bd298c08300c5927ff95af381e0b561d41e5a617
jpantiuchina <patrankyte@gmail.com>
2020-10-06 18:58:49 +0200

    Add 2 duplicated complex methods to BNew.java

--- BNew.java
+++ BNew.java
@@ -13,11 +13,160 @@ class BNew {
     return "B";
   }
 
-  public static void b1(){};
+  public static void b1(){}
 
-  public static void b2(){};
+  public static void b2(){}
 
-  public static void b3(){};
+  public static void b3(){}
+
+  //meaningless
+  public static void complexMethod(int c) {
+    for (int i = 0; i < c; i++) {
+      if (i == 1) {
+        System.out.println(2);
+      }
+      if (i == 2) {
+        System.out.println(2);
+      }
+      if (i == 3) {
+        System.out.println(2);
+      }
+      if (i == 4) {
+        System.out.println(2);
+      }
+      if (i == 5) {
+        System.out.println(5);
+      }
+      if (i == 6) {
+        System.out.println(2);
+      } else if (i == 7) {
+        System.out.println(77);
+      } else if (i == 8) {
+        System.out.println(77);
+      } else if (i == 9) {
+        System.out.println(77);
+      } else if (i == 10) {
+        System.out.println(77);
+      } else if (i == 11) {
+        System.out.println(77);
+      }
+      while (c < 100) {
+        c++;
+        for (int j = 0; j < c; j++) {
+          if (j > i) {
+            System.out.println(j);
+          }
+          if (j > i) {
+            System.out.println(j);
+          }
+          if (j > 10) {
+            System.out.println(i);
+          }
+          if (j > i + 5) {
+            System.out.println(i + 5);
+          }
+        }
+        for (int k = 0; k < i + 10; k++) {
+          if (k < i + 1) {
+            System.out.println(k++);
+          } else if (k > i + 1) {
+            System.out.println(k--);
+          } else if (k == i) {
+            System.out.println("=");
+          }
+        }
+      }
+    }
+  }
+    public static void complexMethodDupl(int c) {
+      for (int i = 0; i < c; i++)
+      {
+        if (i == 1)
+        {
+          System.out.println(2);
+        }
+        if (i == 2)
+        {
+          System.out.println(2);
+        }
+        if (i == 3)
+        {
+          System.out.println(2);
+        }
+        if (i == 4)
+        {
+          System.out.println(2);
+        }
+        if (i == 5)
+        {
+          System.out.println(5);
+        }
+        if (i == 6)
+        {
+          System.out.println(2);
+        }
+        else if (i == 7)
+        {
+          System.out.println(77);
+        }
+        else if (i == 8)
+        {
+          System.out.println(77);
+        }
+        else if (i == 9)
+        {
+          System.out.println(77);
+        }
+        else if (i == 10)
+        {
+          System.out.println(77);
+        }
+        else if (i == 11)
+        {
+          System.out.println(77);
+        }
+        while (c < 100)
+        {
+          c++;
+          for (int j = 0; j < c; j++)
+          {
+            if (j > i)
+            {
+              System.out.println(j);
+            }
+            if (j > i)
+            {
+              System.out.println(j);
+            }
+            if (j > 10)
+            {
+              System.out.println(i);
+            }
+            if (j > i + 5)
+            {
+              System.out.println(i+5);
+            }
+          }
+          for (int k = 0; k < i+10; k++)
+          {
+            if (k < i+1)
+            {
+              System.out.println(k++);
+            }
+            else if ( k > i+1)
+            {
+              System.out.println(k--);
+            }
+            else if (k==i)
+            {
+              System.out.println("=");
+            }
+          }
+        }
+      }
+
+  }
+
 
 
 }
